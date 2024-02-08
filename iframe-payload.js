168fa711f720f3a9b63e44a406e4cd440993500a
Joe Mardell <149815610+JoeMardell@users.noreply.github.com>
2024-02-08 20:34:16 +0000

    Update iframe-payload.js

--- iframe-payload.js
+++ iframe-payload.js
@@ -1,3 +1,12 @@
+/*
+This payload targets change email functionality seen in many web applications. The payload injects an iframe into the page and submits the form containing the updated email address. This will often be enough to go ahead and request a password reset allowing you to takeover the account. 
+
+In this case, the username was also required. The payload sends the username to your burp collaborator address.
+
+I used this on a bug bounty programme to successfully escalate a medium level XSS to critcal.
+*/
+
+
 document.addEventListener("DOMContentLoaded", function() {
     buildFrame();
 })
